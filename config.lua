b01b3549dbe42759ae8d90acb710add6ad84a052
coding team <89742984+arabcodingteam@users.noreply.github.com>
2022-07-06 23:52:12 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.PedsCoords = {
+    [1] = { coords = vector3(-227.45, -1330.24, 29.89), heading = 281.73},
+}
+
+Config.debug = false
+
+Config.Shop = {
+    [1] = {
+        name = "nitrous",
+        price = 1500,
+        amount = 10,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+    [2] = {
+        name = "repairkit",
+        price = 800,
+        amount = 20,
+        info = {},
+        type = "item",
+        slot = 2,
+    },
+}
